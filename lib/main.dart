9b0ed5232905bc6bfe2c1ddbdf381b7e33c1abea
VivekValsalan777 <vivekvalsalan4@gmail.com>
2026-08-18 09:51:47 +0530

    Update main.dart with Firebase initialization and Provider setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'services/firebase_service.dart';
+import 'providers/auth_provider.dart';
+import 'providers/user_provider.dart';
+import 'providers/storage_provider.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseService.initialize();
   runApp(const VibeStreamApp());
+}
+
+class VibeStreamApp extends StatelessWidget {
+  const VibeStreamApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => StorageProvider()),
+      ],
+      child: const App(),
+    );
+  }
 }
